325ef2117cfe159853ccec33149d6ef17c96f7e6
levichen <jiunyi.c@gmail.com>
2014-12-20 20:14:41 +0800

    modify describe

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var users = [
 	{name: '李應群', description: '我是李應群，請人代收'}, 
-	{name: '陳俊毅', description: '我是陳俊毅，我的禮物超好'}, 
+	{name: '陳俊毅', description: '我是宜蘭劉德華，我的禮物超好'}, 
 	{name: '翁聖凱', description: '將心比心，我的禮物最用心'}, 
 	{name: '謝正達', description: '中台灣金城武'}, 
 	{name: '帥哥皓', description: '台中硬邦邦，禮物軟趴趴'}, 
@@ -12,5 +12,5 @@ var users = [
 	{name: '陳毅揚', description: '簡單時尚實用可隨身攜帶上的了廳堂進的了廚房重點是大特價原價遠遠超過500元!!'}, 
 	{name: '林書仲', description: '我禮物絕對不是最爛的'}, 
 	{name: '洪瑞揚', description: '345'}, 
-	{name: '林子修', description: '345'}
+	{name: '林子修', description: '大家好，我是基隆吳孟達'}
 ];
